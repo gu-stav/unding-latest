8440e6e191b407131eb30b66f6cc610d11d1cba0
Gustav Hansen <gustav.hansen@strapi.io>
2023-03-06 18:50:59 +0100

    Shuffle

--- unding.config.js
+++ unding.config.js
@@ -1,12 +1,24 @@
 import GitHub from "@auth/core/providers/github";
 
-export const studio = () => ({
-    auth: {
-        providers: [
-            GitHub({
-                clientId: process.env.GITHUB_ID ?? '71f1c49ab6b41c712e78',
-                clientSecret: process.env.GITHUB_SECRET ?? 'e1771c58fba890cee36f53efff15435bd789fa6e'
-            })
-        ]
-    }
+import article from './content-schemas/article';
+import pages from './content-schemas/pages';
+
+import NewHeader from './NewHeader.svelte';
+
+export const auth = () => ({
+    providers: [
+        GitHub({
+            clientId: process.env.GITHUB_ID ?? '71f1c49ab6b41c712e78',
+            clientSecret: process.env.GITHUB_SECRET ?? 'e1771c58fba890cee36f53efff15435bd789fa6e'
+        })
+    ]
 })
+
+export const schema = [
+    article,
+    pages
+];
+
+export const components = {
+    Header: NewHeader
+};
